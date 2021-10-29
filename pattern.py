ec17347ec06e4d1dd7481c8ce9162138bce7a2f7
Karthik <karthikrpatil.99@gmail.com>
2021-10-29 12:17:20 +0530

    kk

---
+++ pattern.py
@@ -0,0 +1,10 @@
+n = 5  
+# outer loop to handle number of rows  
+for i in range(0, n):  
+    
+        for j in range(0, i + 1):  
+            # printing stars  
+            print("* ", end="")       
+  
+         
+        print()  
